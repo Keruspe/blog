c2aab6eb22b8cff187ad975bf27612cf737b5d8a
Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
2014-09-15 14:34:57 +0200

    copy more subdirs
    Signed-off-by: Marc-Antoine Perennou <Marc-Antoine@Perennou.com>

--- hakyll.hs
+++ hakyll.hs
@@ -20,7 +20,7 @@ main = hakyllWith myConfiguration $ do
         compile compressCssCompiler
 
     -- Copy files and images
-    match ("files/*" .||. "files/*/*" .||. "images/*" .||. "images/*/*") $ do
+    match ("files/*" .||. "files/*/*" .||. "files/*/*/*" .||. "images/*" .||. "images/*/*") $ do
         route   idRoute
         compile copyFileCompiler
 
