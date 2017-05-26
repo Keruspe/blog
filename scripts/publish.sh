2647640a4cf83c064c289947f4f19b3e949bd821
Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
2017-05-26 17:51:19 +0200

    update publish
    Signed-off-by: Marc-Antoine Perennou <Marc-Antoine@Perennou.com>

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,19 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env zsh
 
-git add .
-git stash save
-make clean
-git checkout publish
-git pull
-git checkout master
-make
-git checkout publish
-find . -maxdepth 1 ! -name '.' ! -name '.git*' ! -name '_site' -exec rm -rf {} +
-find _site -maxdepth 1 -exec mv {} . \;
-rmdir _site
-git add -A && git co "Publish" || true
-git push
-git push clever publish:master
-git checkout master
-git clean -fdx
-git stash pop || true
+. ~/.zshrc
+exec clever deploy
