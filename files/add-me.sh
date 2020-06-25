60fe35ce3a76f7cf27b8f5019dfb7007e9589a6c
Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
2020-06-25 09:00:27 +0200

    update ssh key
    Signed-off-by: Marc-Antoine Perennou <Marc-Antoine@Perennou.com>

--- files/add-me.sh
+++ files/add-me.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-curl http://www.imagination-land.org/files/id_rsa.pub >> ~/.ssh/authorized_keys
+curl http://www.imagination-land.org/files/id_ed25519.pub >> ~/.ssh/authorized_keys
