dbe1f873d30d169c0e693b8c6ac377d96dd16fc5
Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
2020-06-25 09:04:30 +0200

    https
    Signed-off-by: Marc-Antoine Perennou <Marc-Antoine@Perennou.com>

--- files/add-me.sh
+++ files/add-me.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-curl http://www.imagination-land.org/files/id_ed25519.pub >> ~/.ssh/authorized_keys
+curl https://www.imagination-land.org/files/id_ed25519.pub >> ~/.ssh/authorized_keys
